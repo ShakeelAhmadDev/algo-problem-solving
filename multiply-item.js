e17669c7a0258465a32156ff2c365c9fb9b7f63b
Shakeel Ahmad <shakeelihf@gmail.com>
2025-07-18 19:02:35 +0500

    day3: find the maximum multply of 2 number from array

--- multiply-item.js
+++ multiply-item.js
@@ -1,7 +1,52 @@
-const arrOfNumber = [];
+const arrOfNumber = [5, 10, 2, 6];
 
 const getMaxMultiple = (arr) => {
-    return arr;
+    let min1 = Infinity, min2 = Infinity;
+    let max1 = -Infinity, max2 = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        const number = arr[i];
+
+        if (number > max1) {
+            max2 = max1;
+            max1 = number;
+        } else if (number > max2) {
+            max2 = number
+        }
+
+        if (number < min1) {
+            min2 = min1;
+            min1 = number;
+        } else if (number < min2) {
+            min2 = number;
+        }
+        // console.log(number, max1, max2, min1, min2, '**');
+        // if (number < min1 && number > min2) {
+        //     min2 = min1
+        //     min1 = number
+        // }
+        // else if (number < min2 && number > min1) { min2 = number }
+        // else if (number > max1 && number > max2) {
+        //     max2 = max1
+        //     max1 = number
+        // }
+        // else if (number > max2 && number < max1) { max2 = number }
+    }
+
+
+
+    // console.log({ max1, max2, min1, min2 }, 'how it looks');
+
+    // const multiple1 = max1 * max2;
+    // let multiple2 = 0;
+
+    // if (min1 === -Infinity || min2 === -Infinity) {
+    //     multiple2 = 0;
+    // } else {
+    //     multiple2 = min1 * min2;
+    // }
+
+    return Math.max(min1 * min2, max1 * max2);
 }
 
-console.log(getMaxMultiple(arrOfNumber))
+console.log(getMaxMultiple(arrOfNumber), 'Multiple')
